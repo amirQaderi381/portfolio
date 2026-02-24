4b200e4c0e7fdabac5289cb634c79d1dcf9bd220
amir <amirqaderi381@gmail.com>
2026-02-24 15:55:24 +0330

    configure MUI RTL with Emotion cache and theme direction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import rtlPlugin from "@mui/stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import { prefixer } from "stylis";
+import "./App.css";
+import { useEffect } from "react";
 
-function App() {
+// Note : create custom theme
+const theme = createTheme({
+  direction: "rtl",
+});
+
+// Note : create RTL cache
+const cacheRtl = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+const App = () => {
+  useEffect(() => {
+    document.title = "وب سایت شخصی امیر قادری";
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CacheProvider value={cacheRtl}>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <h4>وب سایت شخصی</h4>
+        </div>
+      </ThemeProvider>
+    </CacheProvider>
   );
-}
+};
 
 export default App;
