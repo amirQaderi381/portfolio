862116df6ffc8fa081011b5dd944b5d5b3d38938
amir <amirqaderi381@gmail.com>
2026-09-06 18:13:48 +0330

    add Drawer in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,125 +1,33 @@
 import {
-  ConnectWithoutContactRounded,
-  CopyrightRounded,
-  FaceRounded,
-  FavoriteRounded,
-  HomeRounded,
-  MessageRounded,
-  TerminalRounded,
-  TextSnippetRounded,
-} from "@mui/icons-material";
-import {
-  Avatar,
-  Box,
-  Divider,
+  Drawer,
   Grid,
-  Tab,
-  Tabs,
-  Typography,
 } from "@mui/material";
 import { grey } from "@mui/material/colors";
+import { useState } from "react";
+import DrawerContent from "./ui/DrawerContent";
 
 const Sidebar = ({ value, handleChange }) => {
-  const tabProps = (index) => {
-    return {
-      id: `sidebar-tab-${index}`,
-      "aria-controls": `tabpanel-${index}`,
-    };
+  const [openDrawer, SetOpenDrawer] = useState(false);
+  const toggleDrawer = () => {
+    SetOpenDrawer(!openDrawer);
   };
+
   return (
     <Grid size={{ xs: 0, md: 3, lg: 2 }} sx={{ backgroundColor: grey[900] }}>
-      <Box sx={{ justifyContent: "center", textAlign: "center", mt: 2 }}>
-        <Avatar
-          alt="Amir Qaderi"
-          sx={{
-            display: { xs: "none", md: "block" },
-            height: 150,
-            width: 150,
-            margin: "0 auto",
-          }}
-          src={require("../assets/images.jpeg")}
-        >
-          AQ
-        </Avatar>
-        <Typography variant="h6" color="whitesmoke">
-          امیر قادری
-        </Typography>
-        <Typography variant="caption" color="whitesmoke">
-          مدرس و برنامه نویس فول استک
-        </Typography>
-        <Divider variant="middle" color={grey[900]} sx={{ mt: 2 }} />
-
-        <Tabs
-          orientation="vertical"
-          variant="scrollable"
-          scrollButtons="auto"
-          allowScrollButtonsMobile
-          value={value}
-          onChange={handleChange}
-        >
-          <Tab
-            label="صفحه اصلی"
-            icon={<HomeRounded />}
-            iconPosition="start"
-            {...tabProps(0)}
-          />
-          <Tab
-            label="درباره من"
-            icon={<FaceRounded />}
-            iconPosition="start"
-            {...tabProps(1)}
-          />
-          <Tab
-            label="رزومه من"
-            icon={<TextSnippetRounded />}
-            iconPosition="start"
-            {...tabProps(2)}
-          />
-          <Tab
-            label="نمونه کارها"
-            icon={<TerminalRounded />}
-            iconPosition="start"
-            {...tabProps(3)}
-          />
-          <Tab
-            label="نظرات دانشجویان"
-            icon={<MessageRounded />}
-            iconPosition="start"
-            {...tabProps(4)}
-          />
-          <Tab
-            label="ارتباط با من"
-            icon={<ConnectWithoutContactRounded />}
-            iconPosition="start"
-            {...tabProps(5)}
-          />
-        </Tabs>
-
-        <Divider variant="middle" color={grey[900]} sx={{ mt: 2 }} />
-
-        <Box
-          sx={{
-            flexGrow: 1,
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            height: 100,
-          }}
-        >
-          <Typography variant="subtitle2" color="whitesmoke">
-            طراحی شده با{" "}
-            <FavoriteRounded
-              sx={{ verticalAlign: "middle", color: "tomato", height: 20 }}
-            />
-          </Typography>
-
-          <Typography variant="caption" color="whitesmoke" sx={{ mt: 2 }}>
-            کپی رایت 1405{" "}
-            <CopyrightRounded sx={{ verticalAlign: "middle", height: 16 }} />
-          </Typography>
-        </Box>
-      </Box>
+      <DrawerContent value={value} handleChange={handleChange}/>
+      <Drawer
+        open={openDrawer}
+        onClose={() => toggleDrawer(false)}
+        variant="temporary"
+        sx={{
+          "& .MuiDrawer-paper": {
+            width: 300,
+          },
+        }}
+      >
+       {/* Drawer content */}
+       <DrawerContent value={value} handleChange={handleChange}/>
+      </Drawer>
     </Grid>
   );
 };
---
+++ src/components/ui/DrawerContent.jsx
@@ -0,0 +1,125 @@
+import {
+  Avatar,
+  Box,
+  Divider,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
+import { grey } from "@mui/material/colors";
+import {
+  ConnectWithoutContactRounded,
+  CopyrightRounded,
+  FaceRounded,
+  FavoriteRounded,
+  HomeRounded,
+  MessageRounded,
+  TerminalRounded,
+  TextSnippetRounded,
+} from "@mui/icons-material";
+
+const DrawerContent = ({ value, handleChange }) => {
+  const tabProps = (index) => {
+    return {
+      id: `sidebar-tab-${index}`,
+      "aria-controls": `tabpanel-${index}`,
+    };
+  };
+
+  return (
+    <Box sx={{ justifyContent: "center", textAlign: "center", mt: 2 }}>
+      <Avatar
+        alt="Amir Qaderi"
+        sx={{
+          display: { xs: "none", md: "block" },
+          height: 150,
+          width: 150,
+          margin: "0 auto",
+        }}
+        src={require("../../assets/images.jpeg")}
+      >
+        AQ
+      </Avatar>
+      <Typography variant="h6" color="whitesmoke">
+        امیر قادری
+      </Typography>
+      <Typography variant="caption" color="whitesmoke">
+        مدرس و برنامه نویس فول استک
+      </Typography>
+      <Divider variant="middle" color={grey[900]} sx={{ mt: 2 }} />
+
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        scrollButtons="auto"
+        allowScrollButtonsMobile
+        value={value}
+        onChange={handleChange}
+      >
+        <Tab
+          label="صفحه اصلی"
+          icon={<HomeRounded />}
+          iconPosition="start"
+          {...tabProps(0)}
+        />
+        <Tab
+          label="درباره من"
+          icon={<FaceRounded />}
+          iconPosition="start"
+          {...tabProps(1)}
+        />
+        <Tab
+          label="رزومه من"
+          icon={<TextSnippetRounded />}
+          iconPosition="start"
+          {...tabProps(2)}
+        />
+        <Tab
+          label="نمونه کارها"
+          icon={<TerminalRounded />}
+          iconPosition="start"
+          {...tabProps(3)}
+        />
+        <Tab
+          label="نظرات دانشجویان"
+          icon={<MessageRounded />}
+          iconPosition="start"
+          {...tabProps(4)}
+        />
+        <Tab
+          label="ارتباط با من"
+          icon={<ConnectWithoutContactRounded />}
+          iconPosition="start"
+          {...tabProps(5)}
+        />
+      </Tabs>
+
+      <Divider variant="middle" color={grey[900]} sx={{ mt: 2 }} />
+
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: 100,
+        }}
+      >
+        <Typography variant="subtitle2" color="whitesmoke">
+          طراحی شده با{" "}
+          <FavoriteRounded
+            sx={{ verticalAlign: "middle", color: "tomato", height: 20 }}
+          />
+        </Typography>
+
+        <Typography variant="caption" color="whitesmoke" sx={{ mt: 2 }}>
+          کپی رایت 1405{" "}
+          <CopyrightRounded sx={{ verticalAlign: "middle", height: 16 }} />
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default DrawerContent;
